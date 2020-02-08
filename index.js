07780e5dcd7d84093b0c7a8a7ddd61747c55ae20
Nedzad Alibegovic <git@nedzad.dev>
2020-02-08 03:20:18 +0100

    Move API route to /api

--- index.js
+++ index.js
@@ -14,8 +14,9 @@ mongoose.connect(process.env.MONGO);
 const app = express();
 
 app.use(cors());
+app.use(express.static('public'))
 app.use(express.json());
-app.use('/', validationRoute);
-app.use('/', urlRoute)
+app.use('/api/', validationRoute);
+app.use('/api/', urlRoute)
 
 app.listen(process.env.PORT);
