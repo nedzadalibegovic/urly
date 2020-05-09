e6d4c381d38bd5ced83879fdb6c98a1d1b7803ce
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-05-09 22:54:41 +0200

    Add page build 2020-05-09.0

---
+++ docs/precache-manifest.09509f30a1c6c0d85b0a1a0e750cc7b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc89a3ac2b012a5634ba3d25940b9420",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d751cd5a6b50c8591343",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b84cb2ab78a4d2bb1645",
+    "url": "/static/css/main.14b79afd.chunk.css"
+  },
+  {
+    "revision": "d751cd5a6b50c8591343",
+    "url": "/static/js/2.5ffb89bc.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.5ffb89bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b84cb2ab78a4d2bb1645",
+    "url": "/static/js/main.6bf34984.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
