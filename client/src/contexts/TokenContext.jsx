b770d0796421a1f5661d725aa7eea9ce0df2a2c2
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-07-07 22:33:13 +0200

    Add new token interface

--- client/src/contexts/TokenContext.jsx
+++ client/src/contexts/TokenContext.jsx
@@ -3,23 +3,46 @@ import { useHistory } from 'react-router-dom';
 
 export const TokenContext = createContext();
 
-const TokenContextProvider = props => {
+const TokenContextProvider = (props) => {
     const [token, setToken] = useState('');
     const history = useHistory();
 
     const renewToken = async () => {
         const response = await fetch(process.env.REACT_APP_TOKEN, {
-            credentials: 'include'
+            credentials: 'include',
         });
         const { accessToken } = await response.json();
 
         if (!response.ok) return history.push('/login');
 
-        setToken(accessToken);
+        setTokenLS(accessToken);
+    };
+
+    const getTokenLS = () => {
+        const accessToken = localStorage.getItem('accessToken');
+        accessToken ? setToken(accessToken) : renewToken();
+    };
+
+    const setTokenLS = (newToken) => {
+        setToken(newToken);
+        localStorage.setItem('accessToken', newToken);
+    };
+
+    const deleteTokenLS = () => {
+        setToken(null);
+        localStorage.removeItem('accessToken');
     };
 
     return (
-        <TokenContext.Provider value={{ token, setToken, renewToken }}>
+        <TokenContext.Provider
+            value={{
+                token,
+                loadToken: getTokenLS,
+                setToken: setTokenLS,
+                deleteToken: deleteTokenLS,
+                renewToken,
+            }}
+        >
             {props.children}
         </TokenContext.Provider>
     );
