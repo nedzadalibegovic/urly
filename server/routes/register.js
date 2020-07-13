21cf750965b6ccb622f1297a333d27f877f58002
Nedzad Alibegovic <nedzad@nedzad.dev>
2020-07-13 22:07:19 +0200

    Add DELETE method to /register

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const Token = require('../models/token');
 
 router.post('/', async (req, res, next) => {
     try {
@@ -14,6 +16,26 @@ router.post('/', async (req, res, next) => {
     } catch (err) {
         if (err.name === 'ValidationError' || err.code === 11000) res.status(400);
 
+        next(err);
+    }
+});
+
+router.delete('/', async (req, res, next) => {
+    const cookie = req.cookies.refreshToken;
+
+    try {
+        const { _id } = jwt.verify(cookie, process.env.JWT_REFRESH_SECRET);
+        const user = await User.findByIdAndRemove(_id);
+        const token = await Token.findByIdAndRemove(_id);
+
+        if (user === null && token === null) {
+            res.status(400);
+            throw new Error('User doesn\'t exist');
+        }
+
+        res.clearCookie('refreshToken');
+        res.json({ message: `User ${user.username} deleted` });
+    } catch (err) {
         next(err);
     }
 });
